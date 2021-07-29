7916037738d69cc0e93c5be8f545dbb5bb50d6a3
Dominique P <81725988+dpearyer@users.noreply.github.com>
2021-07-28 22:12:53 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import './App.css';
+//importing Components
+import Form from './components/form';
+import TodoList from './components/todolist'
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+    <header>
+      <h1>Dom's Todo List</h1>
+      </header>
+      <Form inputText={inputText} 
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText= {setInputText}/>
+      <TodoList />
+   </div>
+  );
+}
+
+export default App;
